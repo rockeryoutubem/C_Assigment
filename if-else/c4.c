163a4412d27047e6243ba8f6354b9dba76746a7b
rockeryoutubem <106649034+rockeryoutubem@users.noreply.github.com>
2022-06-01 18:35:32 +0530

    Create c4.c
    C program to check whether triangle is valid or not if sides are given

---
+++ if-else/c4.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+int main (){
+int side1,side2,side3;
+printf("Enter the side1,side2,side3");
+scanf("%d%d%d",&side1,&side2,&side3);
+if{((side1+side2)>side3)
+  if((side2+side3)>side1)
+     if((side3+side1)>side2)
+    printf("this is a traingle")}
+else{
+printf("this is not a ")}
+  return 0;
+}
