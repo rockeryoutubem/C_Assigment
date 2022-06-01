8c37ab588ee38e422f5f5de087e73eafcc9e1df8
rockeryoutubem <106649034+rockeryoutubem@users.noreply.github.com>
2022-06-01 19:06:38 +0530

    decimal to binary

---
+++ if-else/switch/bitwise/b2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define INT_SIZE sizeof(int) * 8// ma'am telling about a define discuss in class 
+
+int main()
+{
+    int num, index, i;
+    int bin[INT_SIZE];
+  
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    index = INT_SIZE - 1;
+
+    while(index >= 0)
+    {
+        bin[index] = num & 1;
+
+        index--;
+
+        num >>= 1;
+    }
+
+    printf("Converted binary: ");
+    for(i=0; i<INT_SIZE; i++)
+    {
+        printf("%d", bin[i]);
+    }
+
+    return 0;
+}
