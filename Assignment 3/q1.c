9924086664b0ea664ea416c6daaa2b10455946dc
rockeryoutubem <106649034+rockeryoutubem@users.noreply.github.com>
2022-06-05 12:44:42 +0530

    centigrade converter fahrenheit in c

---
+++ Assignment 3/q1.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+int main (){
+    float c,f;
+    printf("Enter the number of the centigrade");
+    scanf("%f",&c);
+    f = 1.8*c+32;
+    printf("the centigrade convert to Fahrenheit %f :- ",f);
+    return 0;
+}
